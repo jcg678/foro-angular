caabd992b1f09dad11482615c6b44b32d416daad
jcg678 <jcg678@hotmail.es>
2020-04-04 20:50:22 +0200

    Login service get token and user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,9 @@ import { UserService} from '../../services/user.service';
 export class LoginComponent implements OnInit {
   public page_title: string;
   public user: User;
+  public status: string;
+  public identity;
+  public token;
   constructor(private _userService:UserService) {
     this.page_title ="Identificate";
     this.user = new User('','','','','','','ROLE_USER');
@@ -21,6 +24,35 @@ export class LoginComponent implements OnInit {
 
 
   onSubmit(form){
-    console.log(this.user);
+    this._userService.singnup(this.user).subscribe(
+      response => {
+        if(response.user && response.user._id){
+            this.identity = response.user;
+            /////////////////////////Conseguir el token
+          this._userService.singnup(this.user, true).subscribe(
+            response => {
+              if(response.token){
+                  this.token = response.token;
+              }else{
+                this.status = 'error';
+              }
+            },
+            error => {
+              this.status = 'error';
+              console.log(error);
+            }
+          )
+          //////////////////////////
+
+
+        }else{
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(error);
+      }
+    )
   }
 }
