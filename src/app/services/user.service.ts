ccf41ccc40824811fa7372017a840a2f7f61dab1
jcg678 <jcg678@hotmail.es>
2020-04-05 11:38:54 +0200

    Extracción de usuario en servicio

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,7 +6,9 @@ import { global} from './global';
 
 @Injectable()
 export class UserService {
-  public url:string;
+  public url: string;
+  public identity;
+  public token;
 
   constructor(private _http: HttpClient) {
       this.url = global.url;
@@ -33,7 +35,30 @@ export class UserService {
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
 
     return this._http.post(this.url+'login', params, {headers: headers});
+  }
 
+  getIdentity(){
+    let identity = JSON.parse(localStorage.getItem('identity'));
+
+    if(identity && identity != null && identity != undefined && identity != "undefined"){
+        this.identity = identity;
+    }else{
+        this.identity= null;
+    }
+
+    return this.identity;
+  }
+
+  getToken(){
+    let token = localStorage.getItem('token');
+
+    if(token && token != null && token != undefined && token != "undefined"){
+      this.token = token;
+    }else{
+      this.token= null;
+    }
+
+    return this.token;
   }
 
 }
