8ae5b183d7c6f69276c3dd13fd51b46b119b28c1
jcg678 <jcg678@hotmail.es>
2020-04-06 18:27:42 +0200

    Componene ajustes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {HomeComponent} from './components/home/home.component';
+import {UserEditComponent} from './components/user-edit/user-edit.component';
 
 const appRoutes: Routes =[
   {path: '', component: HomeComponent},
   {path: 'inicio', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegisterComponent},
+  {path: 'ajustes', component: UserEditComponent},
   {path: '**', component: LoginComponent}
 ];
 
