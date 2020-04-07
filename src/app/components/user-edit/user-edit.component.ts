96f0f4717077ae3408bc53fd567bd139ae9ea125
jcg678 <jcg678@hotmail.es>
2020-04-07 20:21:09 +0200

    Theme ajustes

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public identity;
+  public token;
+  public status;
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _userService: UserService
+    ) {
+    this.page_title = "Ajustes de usuario";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.user = this.identity;
+  }
 
   ngOnInit() {
   }
