f10122d2c016fb3006a2695044bc7993e803948b
jcg678 <jcg678@hotmail.es>
2020-04-12 13:04:00 +0200

    preparando componente listado topics

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+import { Topic} from '../../models/topic';
+import { TopicService } from '../../services/topic.service';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
+  providers: [TopicService]
 })
 export class TopicsComponent implements OnInit {
+  public page_title: string;
+  public topics: Topic[];
+  public totalPages;
+  public page;
+  public next_page;
+  public prev_page;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _topicService: TopicService
+  ) {
+    this.page_title = 'Temas';
+  }
 
   ngOnInit() {
   }
--- src/app/services/topic.service.ts
+++ src/app/services/topic.service.ts
@@ -43,4 +43,9 @@ export class TopicService {
     const headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', token);
     return this._http.delete(this.url + 'topic/' + id,  { headers: headers});
   }
+
+  getTopics(page = 1): Observable <any> {
+    return this._http.get(this.url + 'topics/' + page);
+  }
+
 }
