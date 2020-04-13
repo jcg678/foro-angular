3f742826f7463fea822c8f6e1d4b753578dc7e95
jcg678 <jcg678@hotmail.es>
2020-04-13 16:50:19 +0200

    Paginación del listado de topics

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -17,6 +17,7 @@ export class TopicsComponent implements OnInit {
   public page;
   public next_page;
   public prev_page;
+  public number_pages;
 
   constructor(
     private _route: ActivatedRoute,
@@ -27,7 +28,17 @@ export class TopicsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getTopics(1);
+    this._route.params.subscribe(params =>{
+      var page= +params['page'];
+      if(!page || page == null  || page == undefined ){
+        page=1;
+        this.prev_page = 1 ;
+        this.next_page = 2 ;
+      }
+      this.getTopics(page);
+
+    });
+
   }
 
   getTopics(page = 1){
@@ -35,8 +46,28 @@ export class TopicsComponent implements OnInit {
       response => {
           if(response.topics){
             this.topics = response.topics;
+            this.totalPages = response.totalPages;
+
+            var number_pages = [];
+            for(var i =1; i<= this.totalPages; i++){
+              number_pages.push(i);
+            }
+            this.number_pages = number_pages;
+            console.log(this.number_pages);
+            if(page > 2){
+              this.prev_page = page-1;
+            }else{
+              this.prev_page = 1;
+            }
+
+            if(page < this.totalPages){
+              this.next_page = page+1;
+            }else{
+              this.next_page = this.totalPages;
+            }
+
           }else{
-            this._router.navigate(['/inicio'])
+            this._router.navigate(['/inicio']);
           }
       },
       error => {
