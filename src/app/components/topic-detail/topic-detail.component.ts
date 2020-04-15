ec3ee0a946f9434a80ee01b27de6c22d8f2160d0
jcg678 <jcg678@hotmail.es>
2020-04-15 19:31:14 +0200

    Maqueta comentarios

--- src/app/components/topic-detail/topic-detail.component.ts
+++ src/app/components/topic-detail/topic-detail.component.ts
@@ -3,22 +3,34 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 
 import { Topic} from '../../models/topic';
 import { TopicService } from '../../services/topic.service';
+import {UserService} from '../../services/user.service';
+
+import {Comments} from '../../models/comments';
+
 
 @Component({
   selector: 'app-topic-detail',
   templateUrl: './topic-detail.component.html',
   styleUrls: ['./topic-detail.component.css'],
-  providers: [TopicService]
+  providers: [TopicService, UserService]
 })
 export class TopicDetailComponent implements OnInit {
 
   public topic: Topic;
+  public comment: Comments;
+  public identity;
+  public token;
+  public status;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _topicService: TopicService
+    private _topicService: TopicService,
+    private _userService: UserService
   ){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.comment = new Comments('','','',this.identity._id);
   }
 
   ngOnInit() {
